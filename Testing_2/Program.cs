dc0abeab1570bc73b9ff16e863b58ad64eec6ccd
Evka <SuperLuck@ya.ru>
2021-04-03 12:02:56 +0300

    Init

---
+++ Testing_2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            string k;
+            string[] str = File.ReadAllLines(@"in.txt");
+            try
+            {
+                x = Convert.ToDouble(str[0]); //  считывает значение в переменную x 
+                y = Convert.ToDouble(str[1]); // считывает значение в переменную y 
+                StreamWriter sr = new StreamWriter(@"out.txt");
+                if ((y >= -x && x <= 0 && x >(-1* Math.Sqrt(y + 2))) || (y >x  && x>0 && x< Math.Sqrt(y+2))) //// A++
+                {
+                    k = " - эта точка принадлежит A ";
+                    sr.WriteLine("x = " + x.ToString() + " and  y = " + y.ToString() + k);
+                }
+
+
+                else if ((y > 0 && y < - x) && (x > (Math.Sqrt(y+2)*(-1)) &&  x<0)) //// B++
+                {
+                    k = " - эта точка принадлежит B ";
+                    sr.WriteLine("x = " + x.ToString() + " and  y = " + y.ToString() + k);
+                }
+                else if (((x >= (-1 * Math.Sqrt(y + 2))) && x <0) && (y <= 0 && y >=x)) //// C++
+
+                {
+                    k = " - эта точка принадлежит C ";
+                    sr.WriteLine("x = " + x.ToString() + " and  y = " + y.ToString() + k);
+                }
+                else if (((x >= (-1 * Math.Sqrt(y + 2))) && (x <= (Math.Sqrt(y + 2)))) && (y < -x && y >= (Math.Pow(x,2) -2))) //// D++
+                {
+                    k = " - эта точка принадлежит D ";
+                    sr.WriteLine("x = " + x.ToString() + " and  y = " + y.ToString() + k);
+                }
+
+                else if ((x >= 0 && (x <=  Math.Sqrt(y + 2))) && (y >= 0 && y <= x)) //// E++
+                {
+                    k = " - эта точка принадлежит E ";
+                    sr.WriteLine("x = " + x.ToString() + " and  y = " + y.ToString() + k);
+                }
+                else if ((y > 0 && y<x) && (x>  Math.Sqrt(y + 2))) //// F++
+                {
+                    k = " - эта точка принадлежит F ";
+                    sr.WriteLine("x = " + x.ToString() + " and  y = " + y.ToString() + k);
+                }
+
+                else if ((x>=0 && x<=-y) && (y <= -x && y < (Math.Pow(x, 2) - 2))) //// G++
+
+                {
+                    k = " - эта точка принадлежит G ";
+                    sr.WriteLine("x = " + x.ToString() + " and  y = " + y.ToString() + k);
+                }
+
+                else if ((x < (-1 * Math.Sqrt(y + 2))) && (y < 0 && y > x)) //// H++
+                {
+                    k = " - эта точка принадлежит H ";
+                    sr.WriteLine("x = " + x.ToString() + " and  y = " + y.ToString() + k);
+                }
+
+                else
+                {
+                    k = " - эта точка не принадлежит ни одной области !";
+                    sr.WriteLine("x = " + x.ToString() + " and  y = " + y.ToString() + k);
+                }
+                sr.Close();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Вводимые значения некорректны. Измените числа в файле!");
+            }
+        }
+    }
+}
---
+++ Testing_Lab_3/Testing_Lab_3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Testing_Lab_3
+{
+    class lab_3
+    {
+        static void Main(string[] args)
+        {
+            logic_B see2 = new logic_B();
+            Console.WriteLine("\r\n///////////////// Задание Б (C#) //////////////////// ");
+            see2.logic_();
+
+            Console.ReadKey();
+        }
+    }
+   
+    class logic_B
+    {
+        double y, x, d, c;
+        public void logic_() // метод
+        {
+            try
+            {
+
+                Console.Write("Введите х - ");
+
+                x = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Введите y - ");
+
+                y = Convert.ToDouble(Console.ReadLine());
+                
+                Console.Write("Введите d - ");
+
+                d = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Введите c - ");
+
+                c = Convert.ToDouble(Console.ReadLine());
+
+                if (y == 0 && x >= 0 && x <= d) // 1 область
+                {
+                    Console.Write("ОБЛАСТЬ №1: ");
+                    Console.WriteLine("X = " + x + "; Y = " + y);
+
+                }
+
+                else if (x<=0 && x >= -d && y==(-d*x+c)) // 2 область
+                {
+                    Console.Write("ОБЛАСТЬ №2: ");
+                    Console.WriteLine("X = " + x + "; Y = " + y);
+
+                }
+
+                else if (x < -d && y == c) // 3 область
+                {
+                    Console.Write("ОБЛАСТЬ №3: ");
+                    Console.WriteLine("X = " + x + "; Y = " + y);
+
+                }
+
+                else // нерассматриваемая область 
+                {
+
+                    Console.WriteLine("X = " + x + "; Y = " + y + "  не принадлежат рассматриваемой области");
+
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Значения записаны некорректно");
+            }
+
+        }
+
+    }
+
+}
